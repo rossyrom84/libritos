f778a0894ae044228807b0c24f3103f8b55e652b
rossyrom84 <rosangely.romero@gmail.com>
2021-06-02 15:34:57 +0200

    fin

--- src/main/java/org/factoriaf5/libritos/controllers/BookController.java
+++ src/main/java/org/factoriaf5/libritos/controllers/BookController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
@@ -30,10 +31,25 @@ public class BookController {
     }
 
     @GetMapping("/books/new")
-    String getForm(Model model){
+    String newBook(Model model){
         Book book = new Book();
         model.addAttribute("book", book);
+        model.addAttribute("title", "Create new book");
         return "books/new";
+    }
+
+    @GetMapping("books/edit/{id}")
+    String editBook(Model model, @PathVariable Long id){
+        Book book = bookService.findById(id);
+        model.addAttribute("book", book);
+        model.addAttribute("title", "Edit book");
+        return "books/edit";
+    }
+
+    @GetMapping("books/delete/{id}")
+    public String remove(@PathVariable Long id) {
+        bookService.delete(id);
+        return "redirect:/books";
     }
 
     @PostMapping("/books/new")
